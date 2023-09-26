c4923fb1ff6cb7831a80dd2e47ef4ebc702c7432
Brian Dang <briandang@umass.edu>
2023-09-26 10:48:13 -0400

    Implemented MinMaxCalculation.java

--- src/Controllers/MinMaxCalculation.java
+++ src/Controllers/MinMaxCalculation.java
@@ -1,3 +1,34 @@
 public class MinMaxCalculation {
-    
+    private ArrayList<Double> data;
+    public MinMaxCalculation(ArrayList<Double> data) {
+        this.data = data;
+    }
+
+    public ArrayList<Double> getData() {
+        return this.data;
+    }
+
+    public void setData(ArrayList<Double> data) {
+        this.data = data;
+    }
+
+    public double minCalc(){
+        double min = getData().get(0);
+        for(double num : data){
+            if(num < min){
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    public double maxCalc(){
+        double max = getData().get(0);
+        for(double num : data){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    }    
 }
